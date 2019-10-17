85b44f2dbe44825ffc2dbfaf642fb5394693ffc8
technologic808 <20627603+technologic808@users.noreply.github.com>
2019-10-17 22:00:45 +0530

    Add Day Seven

---
+++ Day 7/src/com/app/core/Student.java
@@ -0,0 +1,45 @@
+package com.app.core;
+
+import java.util.Date;
+
+
+public class Student {
+	private String prn, name, email, marks;
+	private Date dob;
+	private Course course;
+
+	
+
+	@Override
+	public String toString() {
+		return "Student [prn=" + prn + ", name=" + name + ", email=" + email + ", marks=" + marks + ", dob=" + dob
+				+ "]";
+	}
+
+
+
+
+
+	public Student(String prn, String name, String email, String marks, Date dob, Course course) {
+		super();
+		this.prn = prn;
+		this.name = name;
+		this.email = email;
+		this.marks = marks;
+		this.dob = dob;
+		this.course = course;
+	}
+
+
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		if (obj instanceof Student)
+//		return this.prn == ((Student)obj).prn;
+			return prn.equals(((Student) obj).prn);
+		return false;
+	}
+}
