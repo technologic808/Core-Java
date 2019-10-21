81fab018e76b4d9e2a35c9caefdbed55f553b506
10maycdac <55127938+10maycdac@users.noreply.github.com>
2019-10-22 01:37:23 +0530

    Add files via upload

---
+++ Day 10/Assignment_10.2/schoolexception/StudentHandlingException.java
@@ -0,0 +1,11 @@
+package schoolexception;
+
+public class StudentHandlingException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public StudentHandlingException(String arg0) {
+        super(arg0);
+
+    }
+
+}
