e4fdcd09ab7b263edcfb1e0aea81a76eda2afc28
technologic <20627603+technologic808@users.noreply.github.com>
2019-11-24 20:36:22 +0530

    Handle exceptions gracefully

--- Day 12/com/test/Tester.java
+++ Day 12/com/test/Tester.java
@@ -24,11 +24,13 @@ public class Tester {
 
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            ArrayList<Customer> customerList;
+            ArrayList<Customer> customerList = null;
             try {
                 customerList = (ArrayList<Customer>) restore();
             } catch (FileNotFoundException e) {
                 customerList = new ArrayList<Customer>();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
             // populateList(customerList);
@@ -41,139 +43,123 @@ public class Tester {
             String custType;
 
             while (loop) {
-                menu(); // Displays menu in console
-                option = sc.nextInt();
-                switch (option) {
-                case 1:
-                    // Register new Customer
-                    System.out.println("Enter email: ");
-                    email = sc.next();
-                    System.out.println("Enter password: ");
-                    password = sc.next();
-                    System.out.println("Enter registration date (YYYY-MM-DD): ");
-                    regDate = parse(sc.next());
-                    System.out.println("Enter amount: ");
-                    regAmount = sc.nextDouble();
-                    System.out.println("Enter customer type(SILVER/GOLD/PLATINUM): ");
-                    custType = sc.next();
-                    try {
+                try {
+                    menu(); // Displays menu in console
+                    option = sc.nextInt();
+                    switch (option) {
+                    case 1:
+
+                        // Register new Customer
+                        System.out.println("Enter email: ");
+                        email = sc.next();
+                        System.out.println("Enter password: ");
+                        password = sc.next();
+                        System.out.println("Enter registration date (YYYY-MM-DD): ");
+                        regDate = parse(sc.next());
+                        System.out.println("Enter amount: ");
+                        regAmount = sc.nextDouble();
+                        System.out.println("Enter customer type(SILVER/GOLD/PLATINUM): ");
+                        custType = sc.next();
+
                         customerList.add(new Customer(email, password, regDate, regAmount, CustType.valueOf(custType)));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
 
-                    break;
+                        break;
 
-                case 6:
-                    // Update Address
-                    // Same as Linking new address?
-                    // Just use option to link new address
+                    case 6:
+                        // Update Address
+                        // Same as Linking new address?
+                        // Just use option to link new address
 
-                case 2:
-                    // Link Address to Customer
+                    case 2:
+                        // Link Address to Customer
 
-                    Customer temp = null;
+                        Customer temp = null;
 
-                    try {
                         temp = loginValidation(customerList, sc);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
 
-                    System.out.println("Returned customer after validation is " + temp);
+                        System.out.println("Returned customer after validation is " + temp);
 
-                    System.out.println("Enter City: ");
-                    city = sc.next();
-                    System.out.println("Enter State: ");
-                    state = sc.next();
-                    System.out.println("Enter Country: ");
-                    country = sc.next();
-                    System.out.println("Enter Phone: ");
-                    phoneNo = sc.next();
-                    System.out.println("Enter Address type (Home/Office): ");
-                    addType = sc.next();
+                        System.out.println("Enter City: ");
+                        city = sc.next();
+                        System.out.println("Enter State: ");
+                        state = sc.next();
+                        System.out.println("Enter Country: ");
+                        country = sc.next();
+                        System.out.println("Enter Phone: ");
+                        phoneNo = sc.next();
+                        System.out.println("Enter Address type (Home/Office): ");
+                        addType = sc.next();
 
-                    try {
                         temp.linkAddress(city, state, country, phoneNo, addType);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
 
-                    break;
-                case 3:
-                    // Customer Login
-                    // Checks if email and password match database
-                    try {
+                        break;
+                    case 3:
+                        // Customer Login
+                        // Checks if email and password match database
+
                         loginValidation(customerList, sc);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
 
-                    // Success message
-                    System.out.println("You are successfully logged in!");
+                        // Success message
+                        System.out.println("You are successfully logged in!");
 
-                    break;
-                case 4:
-                    // Unsubscribe Customer
-                    System.out.println("Enter email: ");
-                    email = sc.next();
-                    System.out.println("Enter password: ");
-                    password = sc.next();
+                        break;
+                    case 4:
+                        // Unsubscribe Customer
+                        System.out.println("Enter email: ");
+                        email = sc.next();
+                        System.out.println("Enter password: ");
+                        password = sc.next();
 
-                    try {
                         unsubscribe(email, password, customerList);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
 
-                    // Success
-                    System.out.println("Customer has been unsubscribed!");
+                        // Success
+                        System.out.println("Customer has been unsubscribed!");
 
-                    break;
-                case 5:
-                    // Display all customers by type
-                    System.out.println("Enter customer type(SILVER/GOLD/PLATINUM): ");
-                    custType = sc.next();
-                    for (Customer c : customerList) {
-                        if (c.getType().toString().equals(custType))
-                            System.out.println(c);
-                    }
-
-                case 7:
-                    // Sort Customers by Email using Natural Ordering
-                    Collections.sort(customerList);
-                    break;
-
-                case 8:
-                    // Sort Customers by Reg Date using Custom Ordering
-                    customerList.sort(new Comparator<Customer>() {
-
-                        @Override
-                        public int compare(Customer c1, Customer c2) {
-                            return c1.getRegDate().compareTo(c2.getRegDate());
+                        break;
+                    case 5:
+                        // Display all customers by type
+                        System.out.println("Enter customer type(SILVER/GOLD/PLATINUM): ");
+                        custType = sc.next();
+                        for (Customer c : customerList) {
+                            if (c.getType().toString().equals(custType))
+                                System.out.println(c);
                         }
 
-                    });
-                    break;
+                    case 7:
+                        // Sort Customers by Email using Natural Ordering
+                        Collections.sort(customerList);
+                        break;
 
-                case 10:
-                    System.out.println("Thanks for using this application!");
-                    loop = false;
-                    try {
+                    case 8:
+                        // Sort Customers by Reg Date using Custom Ordering
+                        customerList.sort(new Comparator<Customer>() {
+
+                            @Override
+                            public int compare(Customer c1, Customer c2) {
+                                return c1.getRegDate().compareTo(c2.getRegDate());
+                            }
+
+                        });
+                        break;
+
+                    case 10:
+                        System.out.println("Thanks for using this application!");
+                        loop = false;
+
                         save(customerList);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+
+                        break;
+
+                    default:
+                        System.out.println("Please enter a valid option!");
+                        break;
                     }
-                    break;
 
-                default:
-                    System.out.println("Please enter a valid option!");
-                    break;
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
+            }
+        }
     }
 }
